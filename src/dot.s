f4ae2147aa287d85fd7c7847e44771f9e3d3a9bf
James D. DeBoskey <jddebosk@berkeley.edu>
2022-09-19 21:00:15 -0700

    task4 done

--- src/dot.s
+++ src/dot.s
@@ -18,27 +18,47 @@
 #     this function terminates the program with error code 37
 # =======================================================
 dot:
-
     # Prologue
-
+    ebreak
+    
+    mv t0, x0 # total: starts at 0
+    mv t1, x0 # counter: starts at 0
+       
+    slli a3, a3, 2 # change stride step to byte step counts
+    slli a4, a4, 2 
+    
+    # addi t5 x0 1 # set t5 = 1
+    bgt a2 x0 check_error_1
+    li a0 36
+    j exit
+    
+check_error_1:
+    bgt a3 x0 check_error_2
+    li a0 37
+    j exit
+    
+check_error_2:
+    bgt a4 x0 loop_start
+    li a0 37
+    j exit
 
 loop_start:
-
-
-
-
-
-
-
-
-
-
-
-
+    beq a2, t1, loop_end
+    
+    lw t2 0(a0) # temporary variables to load number from a0
+    lw t3 0(a1) # temp var to load from a1
+    
+    mul t4 t2 t3 # multiply
+    
+    add t0 t0 t4 # add to total
+    
+    add a0 a0 a3 # add by the stride
+    add a1 a1 a4 
+        
+    addi t1 t1 1
+    j loop_start
+    
 loop_end:
-
-
     # Epilogue
-
-
+    mv a0 t0
     jr ra
